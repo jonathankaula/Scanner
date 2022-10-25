b7a3eebbbb45cf44cbe7b129d6f81a85860263e2
Jonathan_Kaula <jonaxkaul@gmail.com>
2022-10-25 17:36:25 +0000

    Create scanner.py

---
+++ scanner.py
@@ -0,0 +1,38 @@
+import scapy
+import sys
+import time
+
+
+
+class Scanner():
+
+	def ethernet():
+
+		'''
+
+		- Destination MAC adress
+		- Source MAC adress
+		- Tag :TPID, PRI, 
+		- Length/Type
+		- Data
+		- FCS 
+
+		'''
+		pass
+
+	def ip():
+		pass
+
+	def icmp():
+
+		pass
+	def tcp():
+		pass
+
+	def arp():
+		pass
+
+
+	def socket():
+		pass
+
